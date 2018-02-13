8495b23f1f4c9a666dd109c2cbc69c993354071c
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-12 17:22:33 -0800

    Asks for first and last name.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,13 @@ using namespace std;
 int main()
 {
   //declare variables
-
+string first;
+string last;
   //get user input
-
+cout<<"What is your first name?\n";
+cin>> first;
+cout<<"What is your last name?\n";
+cin>> last;
   //tell fortune
 
   return 0;
