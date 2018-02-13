91c16e4d205b4e3c21b99c75cf742fa2c20df53d
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-12 17:54:04 -0800

    Tells fortune.

--- main.cpp
+++ main.cpp
@@ -9,12 +9,16 @@ int main()
   //declare variables
 string first;
 string last;
+int lucky;
   //get user input
 cout<<"What is your first name?\n";
 cin>> first;
 cout<<"What is your last name?\n";
 cin>> last;
   //tell fortune
-  cout<<"Welcome,"<<first[0]<<"."<<last[0]<<"., here is your fortune...";
-  return 0;
+  cout<<"Welcome,"<<first[0]<<"."<<last[0]<<"., here is your fortune...\n";
+  //tell fortune
+lucky = first.length();
+cout<<"Your lucky number is "<<lucky<<".";
+return 0;
 }
