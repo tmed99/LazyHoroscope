48c1528110f86c3ff441d3d9637daa24a0cc52a0
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-15 23:48:01 -0800

    Tells fortune.

--- main.cpp
+++ main.cpp
@@ -19,14 +19,14 @@ cin>> last;
   cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune...\n";
   //tell fortune
 lucky = first.length();
-cout<<"Your lucky number is "<<lucky<<"\n";
+cout<<"your lucky number is "<<lucky<<"\n";
 if (first[0] == 97 || first[0] == 65 || first[0] == 69 || first[0] == 101 || first[0] == 73 || first[0] == 105 || first[0] == 79 || first[0] == 111 || first[0] == 87 || first[0] == 117)
 {
-cout<<"you are destined to be famous!";
+cout<<"you are destined to be famous!\n";
 }
 else if ( !(first[0] == 97 || first[0] == 65 || first[0] == 69 || first[0] == 101 || first[0] == 73 || first[0] == 105 || first[0] == 79 || first[0] == 101 || first[0] == 87 || first[0] == 117) )
 {
-cout<<"you should keep a low profile.";
+cout<<"you should keep a low profile.\n";
 }
 if (last.length()-1 == 97 || last.length()-1 == 65 || last.length()-1 == 69 || last.length()-1 == 101 || last.length()-1 == 73 || last.length()-1 == 105 || last.length()-1 == 79 || last.length()-1 == 111 || last.length()-1 == 87 || last.length()-1 == 117)
 {
