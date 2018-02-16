32f15ee11ebb872d2f9a37cb6055471c39adbd01
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-15 23:44:26 -0800

    Tells horoscope.

--- main.cpp
+++ main.cpp
@@ -16,19 +16,19 @@ cin>> first;
 cout<<"What is your last name?\n";
 cin>> last;
   //tell fortune
-  cout<<"Welcome,"<<first[0]<<"."<<last[0]<<"., here is your fortune...\n";
+  cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune...\n";
   //tell fortune
 lucky = first.length();
-cout<<"Your lucky number is "<<lucky<<".\n";
+cout<<"Your lucky number is "<<lucky<<"\n";
 if (first[0] == 97 || first[0] == 65 || first[0] == 69 || first[0] == 101 || first[0] == 73 || first[0] == 105 || first[0] == 79 || first[0] == 111 || first[0] == 87 || first[0] == 117)
 {
-cout<<"you are destined to be famous!\n";
+cout<<"you are destined to be famous!";
 }
-else 
+else if ( !(first[0] == 97 || first[0] == 65 || first[0] == 69 || first[0] == 101 || first[0] == 73 || first[0] == 105 || first[0] == 79 || first[0] == 101 || first[0] == 87 || first[0] == 117) )
 {
-cout<<"you should keep a low profile.\n";
+cout<<"you should keep a low profile.";
 }
-if (lucky-1 == 97 || lucky-1 == 65 || lucky-1 == 69 || lucky-1 == 101 || lucky-1 == 73 || lucky-1 == 105 || lucky-1 == 79 || lucky-1 == 111 || lucky-1 == 87 || lucky-1 == 117)
+if (last.length()-1 == 97 || last.length()-1 == 65 || last.length()-1 == 69 || last.length()-1 == 101 || last.length()-1 == 73 || last.length()-1 == 105 || last.length()-1 == 79 || last.length()-1 == 111 || last.length()-1 == 87 || last.length()-1 == 117)
 {
 cout<<"you have already met your true love.\n";
 }
