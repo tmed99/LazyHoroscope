a235710960f643a84b3eaf95601bf08f8c24c433
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-15 23:24:41 -0800

    Gives fortune.

--- main.cpp
+++ main.cpp
@@ -20,11 +20,7 @@ cin>> last;
   //tell fortune
 lucky = first.length();
 cout<<"Your lucky number is "<<lucky<<".\n";
-if (first[0] == 97 || first[0] == 65) 
-|| (first[0] == 69 || first[0] == 101) 
-|| (first[0] == 73 || first[0] == 105) 
-|| (first[0] == 79 || first[0] == 111) 
-|| (first[0] == 87 || first[0] == 117)
+if (first[0] == 97 || first[0] == 65 || first[0] == 69 || first[0] == 101 || first[0] == 73 || first[0] == 105 || first[0] == 79 || first[0] == 111 || first[0] == 87 || first[0] == 117)
 {
 cout<<"you are destined to be famous!\n";
 }
@@ -32,14 +28,10 @@ else
 {
 cout<<"you should keep a low profile.\n";
 }
-if (last[4] == 97 || last[4] == 65) 
-|| (last[4] == 69 || last[4] == 101) 
-|| (last[4] == 73 || last[4] == 105) 
-|| (last[4] == 79 || last[4] == 111) 
-|| (last[4] == 87 || last[4] == 117)
+if (lucky-1 == 97 || lucky-1 == 65 || lucky-1 == 69 || lucky-1 == 101 || lucky-1 == 73 || lucky-1 == 105 || lucky-1 == 79 || lucky-1 == 111 || lucky-1 == 87 || lucky-1 == 117)
 {
 cout<<"you have already met your true love.\n";
 }
-cout<<"have a good day!";<<endl;
+cout<<"have a good day!\n";
 return 0;
 }
